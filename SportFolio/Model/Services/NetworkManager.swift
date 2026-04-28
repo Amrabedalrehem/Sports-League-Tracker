209db941df0c6e394adf533583748fb326ff7680
shahdashraf2003 <shahdashraf396@gmail.com>
2026-04-28 07:56:18 -0700

    add NetworkManager Alamofire

---
+++ SportFolio/Model/Services/NetworkManager.swift
@@ -0,0 +1,104 @@
+//
+//  NetworkManager.swift
+//  SportFolio
+//
+//  Created by ITI_JETS on 28/04/2026.
+//
+
+
+import Foundation
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    private init() {}
+    
+    
+    func getLeagues(
+        baseURL: String,
+        completion: @escaping (Result<LeaguesResponse, Error>) -> Void
+    ) {
+        APIClient.shared.request(
+            baseURL: baseURL,
+            params: ["met": "Leagues"],
+            responseType: LeaguesResponse.self,
+            completion: completion
+        )
+    }
+    
+    
+    func getUpcomingEvents(
+        baseURL: String,
+        leagueId: Int,
+        from: String,
+        to: String,
+        completion: @escaping (Result<EventsResponse, Error>) -> Void
+    ) {
+        APIClient.shared.request(
+            baseURL: baseURL,
+            params: [
+                "met" : "Fixtures",
+                "leagueId" : leagueId,
+                "from" : from,
+                "to" : to
+            ],
+            responseType: EventsResponse.self,
+            completion: completion
+        )
+    }
+    
+   
+    func getLatestEvents(
+        baseURL: String,
+        leagueId: Int,
+        from: String,
+        to: String,
+        completion: @escaping (Result<EventsResponse, Error>) -> Void
+    ) {
+        APIClient.shared.request(
+            baseURL: baseURL,
+            params: [
+                "met" : "Fixtures",
+                "leagueId" : leagueId,
+                "from" : from,
+                "to" : to
+            ],
+            responseType: EventsResponse.self,
+            completion: completion
+        )
+    }
+    
+   
+    func getTeams(
+        baseURL: String,
+        leagueId: Int,
+        completion: @escaping (Result<TeamsResponse, Error>) -> Void
+    ) {
+        APIClient.shared.request(
+            baseURL: baseURL,
+            params: [
+                "met" : "Teams",
+                "leagueId" : leagueId
+            ],
+            responseType: TeamsResponse.self,
+            completion: completion
+        )
+    }
+    
+   
+    func getTeamDetails(
+        baseURL: String,
+        teamId: Int,
+        completion: @escaping (Result<TeamsResponse, Error>) -> Void
+    ) {
+        APIClient.shared.request(
+            baseURL: baseURL,
+            params: [
+                "met" : "Teams",
+                "teamId" : teamId
+            ],
+            responseType: TeamsResponse.self,
+            completion: completion
+        )
+    }
+}
